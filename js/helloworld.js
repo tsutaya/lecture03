84e344b85fc63713c62f47debfeae83d3dd77881
tsutaya <suzukitatsuya@net76-dhcp156.sfc.keio.ac.jp>
2014-04-24 12:49:59 +0900

    ボタン追加

--- js/helloworld.js
+++ js/helloworld.js
@@ -5,3 +5,12 @@ var helloWorld = function(){
 
 var showHelloWorldButton = document.getElementById("showHelloWorld");
 showHelloWorldButton.addEventListener("click", helloWorld);
+
+
+var helloWorld2 = function(){
+	var message = document.getElementById("hellopooooo");
+	message.setAttribute("class", "");
+};
+
+var showHelloWorld2Button = document.getElementById("showHellopooooo");
+showHelloWorld2Button.addEventListener("click", helloWorld2);
